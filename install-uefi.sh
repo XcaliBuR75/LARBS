63c981a1607895b2a0a47ddc2a3484af170b1074
XcaLiBuR75 <ghernan75@gmail.com>
2022-05-07 17:02:01 -0300

    Updated

--- install-uefi.sh
+++ install-uefi.sh
@@ -14,7 +14,7 @@ echo "127.0.1.1 arch.localdomain arch" > /etc/hosts
 echo root:password | chpasswd
 
 # Update archlinux keyring
-pacman -Sy archlinux-keyring
+pacman -Sy --noconfirm archlinux-keyring
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
